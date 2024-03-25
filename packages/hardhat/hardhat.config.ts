5deefe350e3d2dd2db5ca4fa0c08b61b025a72dd
lintuming <lintuming@bytedance.com>
2024-03-25 20:56:30 +0800

    chore: hardhat config

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,8 +1,55 @@
+import * as dotenv from 'dotenv'
 import { type HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox'
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-chai-matchers'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import '@nomicfoundation/hardhat-verify'
+import 'hardhat-deploy'
+import 'hardhat-deploy-ethers'
+dotenv.config()
+
+// If not set, it uses ours Alchemy's default API key.
+// You can get your own at https://dashboard.alchemyapi.io
+const providerApiKey =
+  process.env.ALCHEMY_API_KEY ?? 'oKxs-03sij-U_N0iOlrSsZFr29-IqbuF'
+// If not set, it uses the hardhat account 0 private key.
+const deployerPrivateKey =
+  process.env.DEPLOYER_PRIVATE_KEY ??
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+// If not set, it uses ours Etherscan default API key.
+const etherscanApiKey =
+  process.env.ETHERSCAN_API_KEY ?? 'DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW'
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.24'
+  solidity: '0.8.25',
+  networks: {
+    // View the networks that are pre-configured.
+    // If the network you are looking for is not here you can add new network settings
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+        enabled: process.env.MAINNET_FORKING_ENABLED === 'true'
+      }
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey]
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  verify: {
+    etherscan: {
+      apiKey: `${etherscanApiKey}`
+    }
+  }
 }
 
 export default config
